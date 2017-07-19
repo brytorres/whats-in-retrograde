8b090e9af2b745c98b75315ef3464de237da3c9b
brytorres <dev@bryantorres.pro>
2017-07-19 16:16:24 -0400

    Iterate though dateInfo contents with fetchRetrograde

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,8 @@
 // Fetch retrograde dateInfo for whole file.
-$.getScript('/assets/js/retrogradeData.js', function(){
+// $.getScript('/assets/js/retrogradeData.js', function(){
+
 // Listen for form submit
-// document.getElementById('myForm').addEventListener('submit', saveBookmark);
-
-// Import retrograde date data
-// var dateInfo = document.createElement('script');
-// dateInfo.src = '/assets/js/retrogradeData.js';
-// document.head.appendChild(dateInfo);
-// console.log(dateInfo)
-// 
-
-    // script is now loaded and executed.
-    // put your dependent JS here.
-    console.log(dateInfo.jan01)
-
+// document.getElementById('retrogradeResults').addEventListener('submit', saveBookmark);
 
 // Display current time and date as [xx-xx-xxxx]
 var currentDate = new Date();
@@ -44,13 +33,69 @@ function numToMon(monthNum){
 // Generate todays date into string for comparison
 var today = numToMon(monthNum) + currentDateDay;
 
-// 2017 planets retrograde data 
-// var retrograde = {}
-
 // Fetch current retrograde data
 function fetchRetrograde(){
+    // Get Output ID
+    var currentlyRetrograde = document.getElementById('currentlyRetrograde');
+    // var todaysDate = today;
+    // console.log(dateInfo.jan01)
+    // Build Output
+    currentlyRetrograde.innerHTML = '';
 
+    // Iterate through all dates
+    for(var key in dateInfo){
+        if (!dateInfo.hasOwnProperty(key)) {
+            continue;
+        }
+        var planets = dateInfo[key].planets;
+        var direction = dateInfo[key].direction;
+        var time = dateInfo[key].time;
+        var sign = dateInfo[key].sign;
+        var degrees = dateInfo[key].degrees;
+        var minutes = dateInfo[key].minutes;
+
+        for(var i = 0; i < planets.length;i++){
+            // if(typeof(time[i] == undefined)){
+            //     var time = ['', 'No Transition Occuring', 'No Transition Occuring', 'No Transition Occuring', 'No Transition Occuring', 'No Transition Occuring', 'No Transition Occuring', 'No Transition Occuring', 'No Transition Occuring', ];
+            // } if(typeof(sign == undefined)){
+            //     var sign = [''];
+            // } if(typeof(degress == undefined)){
+            //     var degress = '';
+            // } if(typeof(minutes == undefined)){
+            //     var minutes = '';
+            // }
+            currentlyRetrograde.innerHTML +=    '<div class="well">' +
+                                                '<h3>' +planets[i]+ '<h3>' + '<br>' + '<br>' +
+                                                '<p>' +direction[i]+ '</p>' + '<br>' +
+                                                '<p>' +time[i]+ '</p>' + '<br>' +
+                                                '<p>' +sign[i]+ '</p>' + '<br>' +
+                                                '<p>' +degrees[i]+ '</p>' + '<br>' +
+                                                '<p>' +minutes[i]+ '</p>' + '<br>' + 
+                                                '</div>'
+                                                
+            
+        }
+    }
+        // var planet = dateInfo[i].planet;
+    //     var direction = dateInfo[i].direction;
+    //     var time = dateInfo[i].time;
+    //     var sign = dateInfo[i].sign;
+    //     var degrees = dateInfo[i].degrees;
+    //     var minutes = dateInfo[i].minutes;
+
+    //     bookmarksResults.innerHTML +=   '<div class="well">' +
+    //                                     '<h3>' +planet+ '<h3>' + '<br>' + '<br>' +
+    //                                     '<p>' +direction+ '</p>' + '<br>' +
+    //                                     '<p>' +time+ '</p>' + '<br>' +
+    //                                     '<p>' +sign+ '</p>' + '<br>' +
+    //                                     '<p>' +degrees+ '</p>' + '<br>' +
+    //                                     '<p>' +minutes+ '</p>' + '<br>'
+    //     // Match today with interation results
+            
+    //         // 
+    // }
 }
+
 
 // Fetch planets currently in retrograde
 function currentlyRetrograde(){
@@ -74,4 +119,4 @@ function byDateResults(){
 //     // Regex needed?   
 //     return true;
 // }
-});
+// });
--- assets/js/retrogradeData.js
+++ assets/js/retrogradeData.js
@@ -1,11 +1,11 @@
 // Variable that holds objects, each object is a day of the year.
 var dateInfo = {
     jan01: {
-        planet: ['Mercury', 'Jupiter', 'Saturn', 'Pluto'],
+        planets: ['Mercury', 'Jupiter', 'Saturn', 'Pluto'],
         direction: ['Retrograde', 'In RX Zone', 'In RX Zone', 'In Rx Zone'],
-        time: [''],
-        sign: [''],
-        degrees: [''],
-        minutes: [''],
+        time: ['07:11 AM'],
+        sign: ['Cap'],
+        degrees: [],
+        minutes: [],
     }
 }
