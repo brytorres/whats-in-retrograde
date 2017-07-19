42625d8289392c45a7bd69ce06554b40730a5594
brytorres <dev@bryantorres.pro>
2017-07-19 00:42:30 -0400

    Prepare main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,59 @@
 // Listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-// Fetch Current Retrograde Data
-function fetchRetrograde(){}
+// 2017 planets retrograde data 
+var planetData = {
+    mercury: {
+        
+    },
+    venus: {
+
+    },
+    mars: {
+
+    },
+    jupiter: {
+
+    },
+    saturn: {
+
+    },
+    uranus: {
+
+    },
+    neptune: {
+
+    },
+    pluto: {
+
+    }
+}
+
+// Fetch current retrograde data
+function fetchRetrograde(){
+
+}
+
+// Fetch planets currently in retrograde
+function currentlyRetrograde(){
+
+}
+
+function nextRetrogrades(){
+
+}
+
+function byDateResults(){
+
+}
+
+// Validate form
+function validateForm(date, planet){
+    if(!date || !planet){
+        alert('Please fill in the form.');
+        return false;
+
+    // Regex needed?
+    
+    return true;
+}
